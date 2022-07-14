62e74ac8b057fa46b144d7afb27ee9269b557da0
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-14 15:46:57 -0400

    Updated express_server.js

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to the default landing zone!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
