5293662e1db0fe6ebf5540b9abef6a2955b15d02
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-14 21:06:42 -0400

    Updated express_server.js

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,8 +18,18 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL }; //?
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
